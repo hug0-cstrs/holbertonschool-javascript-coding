d31bcd78507c54364b11f982edf89ea607781b57
Hugo Castéras <130242027+hug0-cstrs@users.noreply.github.com>
2023-09-19 10:53:56 +0000

    update file

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/:id' + process.argv[2] + '/';
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
