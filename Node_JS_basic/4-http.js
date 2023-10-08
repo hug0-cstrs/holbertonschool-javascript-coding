5b88a56f0f4376147e688d838ee093ee187eacb6
Hugo Castéras <130242027+hug0-cstrs@users.noreply.github.com>
2023-10-08 09:13:49 +0000

    update file

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,10 +1,11 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Holberton School!\n');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 
-app.listen(1245);
+app.listen(1245, '127.0.0.1');
 
 module.exports = app;
