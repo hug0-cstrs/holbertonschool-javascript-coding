c28a48537ee752000e0bc52682b1f383d27c65dc
Hugo Castéras <130242027+hug0-cstrs@users.noreply.github.com>
2023-10-08 09:40:16 +0000

    update file, add comment for a better understanding

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,49 +1,63 @@
 const http = require('http');
 const fs = require('fs');
-const url = require('url');
 
+// Create a server instance
 const app = http.createServer((req, res) => {
-  const { pathname } = url.parse(req.url);
-
-  if (pathname === '/') {
+  // Handle requests for the root URL
+  if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Holberton School!\n');
-  } else if (pathname === '/students') {
-    const databaseFilePath = process.argv[2];
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') { // Handle requests for the '/students' URL
+    // Read the database file from the command line arguments
+    const database = process.argv[2];
+    // Read the contents of the database file
+    fs.readFile(database, 'utf8', (err, data) => {
+      if (err) {
+        // Handle errors while reading the database file
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+      } else {
+        // Parse the student data from the database file
+        const students = data.split('\n').filter((line) => line !== '');
+        const fields = {};
 
-    if (!databaseFilePath) {
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.end('Database file not provided.\n');
-    } else {
-      fs.readFile(databaseFilePath, 'utf8', (err, data) => {
-        if (err) {
-          res.writeHead(500, { 'Content-Type': 'text/plain' });
-          res.end('Internal Server Error.\n');
-        } else {
-          const students = data
-            .split('\n')
-            .filter((line) => line.trim() !== '');
+        // Group the students by field of study
+        students.forEach((student) => {
+          const [firstname, , , field] = student.split(',');
+          if (!fields[field]) {
+            fields[field] = {
+              count: 0,
+              list: [],
+            };
+          }
+          fields[field].count += 1;
+          fields[field].list.push(firstname);
+        });
 
-          res.writeHead(200, { 'Content-Type': 'text/plain' });
-          res.write('This is the list of our students\n');
+        // Send the response with the list of students grouped by field of study
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.write('This is the list of our students\n');
+        res.write(`Number of students: ${students.length}\n`);
 
-          const countStudents = require('./3-read_file_async');
-          countStudents(databaseFilePath, (error, result) => {
-            if (error) {
-              res.end(`Error: ${error.message}\n`);
-            } else {
-              res.end(result);
-            }
-          });
+        for (const field in fields) {
+          if (Object.prototype.hasOwnProperty.call(fields, field)) {
+            res.write(`Number of students in ${field}: ${fields[field].count}. List: ${fields[field].list.join(', ')}\n`);
+          }
         }
-      });
-    }
-  } else {
+
+        res.end();
+      }
+    });
+  } else { // Handle all other requests
     res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found\n');
+    res.end('Not Found');
   }
 });
 
-app.listen(1245);
+// Start the server and listen on port 1245
+app.listen(1245, () => {
+  console.log('Server listening on port 1245');
+});
 
+// Export the server instance for testing purposes
 module.exports = app;
